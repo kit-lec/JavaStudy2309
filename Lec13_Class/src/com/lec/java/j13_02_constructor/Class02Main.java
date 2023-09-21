58dbe114e9185d8e782b12b11a8a29557f4bebb8
kit-lec <kit_lec@hotmail.com>
2023-09-21 17:56:02 +0900

    230921_0

---
+++ Lec13_Class/src/com/lec/java/j13_02_constructor/Class02Main.java
@@ -0,0 +1,24 @@
+package com.lec.java.j13_02_constructor;
+
+
+public class Class02Main {
+
+	public static void main(String[] args) {
+		System.out.println("클래스 연습");		
+		
+		// TODO
+		
+		System.out.println("프로그램 종료");
+	} // end main()
+
+} // end class Class02Main
+
+
+
+
+
+
+
+
+
+
---
+++ Lec13_Class/src/com/lec/java/j13_07_Lombok/Model.java
@@ -0,0 +1,12 @@
+package com.lec.java.j13_07_Lombok;
+
+
+
+
+public class Model {
+	private String title;
+	private int old;
+	private boolean isAdult;
+	private char gender;
+	private short size;
+}
